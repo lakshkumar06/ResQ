edfccd742c60aee2e2888e63963e587a71aaa4e3
lakshkumar06 <laksh150034@gmail.com>
2025-02-16 08:54:43 -0600

    COnnected

---
+++ frontend/src/components/Server.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+
+const Server = ({ setConfigData }) => {
+  useEffect(() => {
+    const fetchConfigData = async () => {
+      try {
+        const response = await fetch('https://api.github.com/gists/50bce786e694e5f632bbd0ecd0e90b96', {
+          headers: {
+            Authorization: `token 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`, // Add your token here
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`Error fetching data: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const _configData = JSON.parse(data.files['Firestations.json'].content);
+        setConfigData(_configData);
+        console.log(_configData);
+      } catch (error) {
+        console.error('Error fetching config data:', error);
+      }
+    };
+
+    fetchConfigData();
+  }, [setConfigData]);
+
+  return null; // This component does not need to render anything
+};
+
+export default Server;
